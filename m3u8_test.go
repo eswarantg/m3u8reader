b5196e5b5235fc39a4404c53b62605f1bcbb78de
Guna Anban G <gg@synamedia.com>
2021-11-26 13:11:24 +0530

    separate preloadHint testcase added

--- m3u8_test.go
+++ m3u8_test.go
@@ -9,6 +9,34 @@ import (
 )
 
 func Test_M3u8(t *testing.T) {
+	tests := []string{"test/ll_hls_byte_range.m3u8",
+		"test/ll_hls_delta_update.m3u8",
+		"test/ll_hls_pl.m3u8",
+		"test/index_new.m3u8",
+		"test/index_new_Variant_450k.m3u8",
+		"test/tv5.m3u8",
+		"test/tv5_TS-50002_1_video.m3u8",
+		"test/master.m3u8",
+	}
+	for i, file := range tests {
+		fmt.Printf("\n********* Test %v - %v ************", i, file)
+		f, err := os.Open(file)
+		if err != nil {
+			t.Errorf("Unable to open file")
+			return
+		}
+		defer f.Close()
+		manifest := m3u8reader.M3U8{}
+		_, err = manifest.Read(f)
+		if err != nil {
+			t.Errorf(err.Error())
+			return
+		}
+		fmt.Print(manifest.String())
+	}
+}
+
+func Test_PreloadHintEntry(t *testing.T) {
 	tests := []string{
 		"test/submani.m3u8",
 	}
@@ -26,7 +54,6 @@ func Test_M3u8(t *testing.T) {
 			t.Errorf(err.Error())
 			return
 		}
-		fmt.Print(manifest.String())
 		fmt.Printf("\n preloadHintEntry is: \n")
 		fmt.Print(manifest.PreloadHintEntry())
 	}
